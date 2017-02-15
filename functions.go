9d65e22e1312d4b204a3f6d58fe689b125a87de6
robjporter <robjporter@outlook.com>
2017-02-15 10:17:15 +0000

    Made functions public

--- functions.go
+++ functions.go
@@ -2,28 +2,40 @@ package functions
 
 import (
 	"html/template"
+	"os"
 )
 
 var (
 	Bold = func(content string) (template.HTML, error) {
-		return template.HTML("<b>" + content + "</b>"),nil
+		return template.HTML("<b>" + content + "</b>"), nil
 	}
 	Italic = func(content string) (template.HTML, error) {
-		return template.HTML("<i>" + content + "</i>"),nil
+		return template.HTML("<i>" + content + "</i>"), nil
 	}
 	Underline = func(content string) (template.HTML, error) {
-		return template.HTML("<u>" + content + "</u>"),nil
+		return template.HTML("<u>" + content + "</u>"), nil
 	}
 )
 
-func RemoveDuplicates(a []string) []string { 
-	result := []string{} 
-	seen := map[string]string{} 
-	for _, val := range a { 
-		if _, ok := seen[val]; !ok { 
-			result = append(result, val) 
-			seen[val] = val 
-		} 
-	} 
-	return result 
-} 
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func RemoveDuplicates(a []string) []string {
+	result := []string{}
+	seen := map[string]string{}
+	for _, val := range a {
+		if _, ok := seen[val]; !ok {
+			result = append(result, val)
+			seen[val] = val
+		}
+	}
+	return result
+}
